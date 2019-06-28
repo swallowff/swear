8d6e95c562b25f42c023e7190afe8bcb4027be4f
swallowff <swallowff@163.com>
2019-06-28 16:14:22 +0800

    增加wrapper

--- modules/core/src/main/java/cn/swallowff/modules/core/config/web/WebConfig.java
+++ modules/core/src/main/java/cn/swallowff/modules/core/config/web/WebConfig.java
@@ -1,17 +1,6 @@
 package cn.swallowff.modules.core.config.web;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.module.SimpleModule;
-import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.text.SimpleDateFormat;
-import java.util.List;
-import java.util.TimeZone;
 
 @Configuration
 public class WebConfig {
